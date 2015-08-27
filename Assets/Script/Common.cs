dc93053be14ce1b0df57191f78b28f728419b14a
jyzgo <jiangyizheng@magictavern.com>
2015-08-27 19:32:03 +0800

    init a match3 demo

---
+++ Assets/Script/Common.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public static class Constants
+{
+	public const int MAX_ROWS = 17;
+	public const int MAX_COLS = 17;
+	public const float CELL_SIDE = 1.4f;
+	public const float CELLS_LEFT = -5.65f;
+	public const float CELLS_BOTTOM = -4.7f;
+	public const float SWAP_TIME = 0.3f;
+	public const float FORM_TIME = 0.33f;
+
+	public const float CELL_ELIM_TIME = 0.13f;
+
+	public const int CORLOR_NUM = 7;
+
+	public const bool DEBUG_MODE = true;
+}
+
+public enum CellColor {
+	None = 0,
+	Red  = 1,
+	Blue = 2,
+	Green = 3,
+	Brown = 4,
+	Purple = 5,
+	Yellow = 6,
+	All
+};
+
+public enum CellType
+{
+	None,
+	Brick,
+	Bomb
+}
+
+public enum BombType
+{
+	None,
+	Horizontal,
+	Vertical,
+	Square,
+	Fish,
+	Color,
+	Coloring
+}
---
+++ Assets/Script/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Init();
+	}
+	
+	void Init()
+	{
+		//init controllers
+		//mvc mvc
+
+
+	}
+
+	void InitModels()
+	{
+		
+	}
+}
