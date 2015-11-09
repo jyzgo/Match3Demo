e487287bd4c5ad4c76f171d00a79458d70812576
jiang yizheng <8559509@163.com>
2015-11-10 00:26:45 +0800

    update version and up some tools

---
+++ Assets/Editor/Tools/AddChild.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEditor;
+using System.Collections;
+using UnityEngine;
+
+public class AddChild : ScriptableObject
+{
+	[MenuItem ("GameObject/Add Child ^n")]
+	static void MenuAddChild()
+	{
+		Transform[] transforms = Selection.GetTransforms(SelectionMode.TopLevel | SelectionMode.OnlyUserModifiable);
+		
+		foreach(Transform transform in transforms)
+		{
+			GameObject newChild = new GameObject("_Child");
+			newChild.transform.parent = transform;
+		}
+	}
+}
